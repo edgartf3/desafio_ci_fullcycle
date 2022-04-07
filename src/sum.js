94a4111d4282582ed060315b59e944f9bc5db007
Edgar <edgar@onsist.com.br>
2022-04-07 09:11:24 -0300

    More functions

--- src/sum.js
+++ src/sum.js
@@ -6,4 +6,21 @@ function time(a, b) {
   return a * b;
 }
 
+function subs(a, b) {
+  return a - b;
+}
+
+function div(a, b) {
+  return a / b;
+}
+
+function mod(a, b) {
+  return a / b;
+}
+
+function mod2(a, b) {
+  return a / b;
+}
+
+
 module.exports = sum
